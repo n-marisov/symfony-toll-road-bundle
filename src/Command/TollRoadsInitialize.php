f82dee947628e03dfc33ac83df8f43d64488c0b0
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-19 20:20:28 +0300

    2

--- src/Command/TollRoadsInitialize.php
+++ src/Command/TollRoadsInitialize.php
@@ -3,12 +3,11 @@
 namespace Maris\Symfony\TollRoad\Command;
 
 use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'toll_road:init')]
+//#[AsCommand(name: 'toll_road:init')]
 class TollRoadsInitialize extends Command
 {
 
